9e83dfc19ea978243d73277c2da1ac98df7f4db9
Bas Mutsaers <bm169900@edu.rocwb.nl>
2018-03-01 10:50:04 +0100

    Addes settingsPage

--- HRInvoiceApp/HRInvoiceApp/MainPage.xaml.cs
+++ HRInvoiceApp/HRInvoiceApp/MainPage.xaml.cs
@@ -8,14 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using SQLiteNetExtensions;
+using Windows.Gaming.Input;
 
 namespace HRInvoiceApp
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : TabbedPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this,false);
             SQLiteAsyncConnection db = App.Database.GetInstance();
         }
 	}
