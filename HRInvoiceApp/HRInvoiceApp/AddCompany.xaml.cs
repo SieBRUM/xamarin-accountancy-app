332743b9d4e42156e44526797e4e714e58fc475d
Siebren Kraak <siebrenkraak@hotmail.com>
2018-03-05 10:07:14 +0100

    master => Siebren (#18)
    * Siebren => master (#15)
    
    * Add navigation to pages, toolbar icons. Added screens (#12) (#13)
    
    * Database changes.
    Relocated input validation to seperate file.
    Added basic functionality to AddDepartment page.
    
    * Fixed warnings
    
    * Added basic functionality to AddCompany (#17)

--- HRInvoiceApp/HRInvoiceApp/AddCompany.xaml.cs
+++ HRInvoiceApp/HRInvoiceApp/AddCompany.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HRInvoiceApp.Tables;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,34 @@ namespace HRInvoiceApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddCompany : ContentPage
 	{
-		public AddCompany ()
+        SQLiteAsyncConnection db;
+        Company company;
+        public AddCompany ()
 		{
 			InitializeComponent ();
             SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            db = App.Database.GetInstance();
+
+            Task.Run(async () =>
+            {
+                company = await db.Table<Company>().FirstOrDefaultAsync();
+                company = new Company();
+            });
         }
+
+        void saveCompany(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                company.CompanyName = companyName.Text;
+                company.AddressAddition = AddressAddition.Text;
+                company.City = City.Text;
+                company.Zipcode = ZipCode.Text;
+
+                await db.InsertOrReplaceAsync(company);
+                await DisplayAlert("Succes", "Instellingen succesvol opgeslagen.", "OK");
+            });
+        }
+            
 	}
 }
