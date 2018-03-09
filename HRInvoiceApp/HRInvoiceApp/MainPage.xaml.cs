91579d6e0fdb9db6494de07792036e7d4d8aa1ee
SieBRUM <siebrenkraak@hotmail.com>
2018-03-09 09:14:39 +0100

    Removed navigation tab.

--- HRInvoiceApp/HRInvoiceApp/MainPage.xaml.cs
+++ HRInvoiceApp/HRInvoiceApp/MainPage.xaml.cs
@@ -16,8 +16,6 @@ namespace HRInvoiceApp
             Children.Add(new NavigationPage(new Assignment_Page {  }) {Title = "Opdrachtoverzicht" });
             Children.Add(new NavigationPage(new ListPage { }) {Title = "Maandoverzicht" });
             Children.Add(new NavigationPage(new SettingsPage { }) {Title = "Instellingen" });
-            Children.Add(new NavigationPage(new AddAssignment { }) {Title = "Add assigment" });
-
         }
     }
 }
